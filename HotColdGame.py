7232b1131312ece0af7a3262c97a576212b42ce4
Jackson Tuttle <jacksontuttlemail@gmail.com>
2023-03-16 09:49:27 -0500

    Got a circle moving

--- HotColdGame.py
+++ HotColdGame.py
@@ -1,84 +1,49 @@
-#!/usr/bin/env python
+import os, sys, math, pygame, pygame.mixer
+from pygame.locals import *
 
-"""Proof of concept gfxdraw example"""
+black = (0,0,0)
+red = (255,0,0)
+green = (0,255,0)
+blue = (0,0,255)
+white = (255,255,255)
+run_me = True
 
-import pygame
-import pygame.gfxdraw
-import random
+screen_size = screen_width, screen_height = 600, 400
+screen = pygame.display.set_mode(screen_size)
+pygame.display.set_caption('ha ha ha ha ha')
 
-# game data that never changes
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-WHITE = (255, 255, 255)
-YELLOW = (255, 233, 0)
+clock = pygame.time.Clock()
+fps_limit = 60
 
-SCREEN_SIZE = 800
-SCREEN = pygame.display.set_mode((SCREEN_SIZE, SCREEN_SIZE))
+#circle 
+colorcircle = (red)
+posx = 300
+posy = 200
+circle = pygame.draw.circle(screen, colorcircle, (posx, posy), 50)
 
-pygame.init()
-screen = pygame.display.set_mode((500, 500))
-screen.fill((0, 0, 0))
-s = pygame.Surface(screen.get_size(), pygame.SRCALPHA, 32)
-pygame.draw.line(s, (0, 0, 0), (250, 250), (250 + 200, 250))
-
-width = 1
-
-posx = 250
-posy = 250
-
-"""
-def play_music():
+while run_me:
+    clock.tick(fps_limit) 
 
 
-def set_difficulty(level, difficulty):
-
-
-def display_text():
-
-
-def set_random_position():
-
-
-def set_circle_colors():
-
-    
-def play_game():
-"""
-
-def draw_circle():
-    circle = pygame.draw.circle(screen, 'WHITE', (posx, posy), 40)
-    circle
-
-def main():
-    """
-    for a_radius in range(width):
-        radius = 200
-        pygame.gfxdraw.aacircle(s, 250, 250, radius - a_radius, (0, 0, 0))
-
-    screen.blit(s, (0, 0))
-    """
-    draw_circle()
-
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run_me = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                posx = posx - 10
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                posx = posx + 10
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                posy = posy - 10
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_DOWN:
+                posy = posy + 10
+    # fill the screen with black (otherwise, the circle will leave a trail)
+    screen.fill(black)
+    # redraw the circle
+    pygame.draw.circle(screen, colorcircle, (posx, posy), 50)
     pygame.display.flip()
 
-    try:
-        while True:
-            event = pygame.event.wait()
-            if event.type == pygame.QUIT:
-                break
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    posx = posx + 10
-                if event.key == pygame.K_ESCAPE or event.unicode == "q":
-                    break
-                screen.fill(BLACK)
-                pygame.draw.circle(screen, 'WHITE', (posx, posy), 40)
-                pygame.display.flip()
-    finally:
-        pygame.quit()
-
-
-if __name__ == "__main__":
-    main()
+pygame.quit()
