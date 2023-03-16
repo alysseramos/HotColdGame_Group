1452a1f9f3569a7c1ed0e17552aac51d25e8afff
RohanOelofse <rohanoelofse1@gmail.com>
2023-03-16 10:16:29 -0500

    Step 2 completed

--- HotColdGame.py
+++ HotColdGame.py
@@ -1,48 +1,72 @@
 import os, sys, math, pygame, pygame.mixer
 from pygame.locals import *
+import random
 
-black = (0,0,0)
-red = (255,0,0)
-green = (0,255,0)
-blue = (0,0,255)
-white = (255,255,255)
-run_me = True
+black = (0, 0, 0)
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+white = (255, 255, 255)
 
-screen_size = screen_width, screen_height = 600, 400
+
+screen_size = screen_width, screen_height = 800, 800
 screen = pygame.display.set_mode(screen_size)
-pygame.display.set_caption('ha ha ha ha ha')
 
-clock = pygame.time.Clock()
-fps_limit = 60
 
-#circle 
-colorcircle = (red)
+
+# fps_limit = 60
+
+colorcircle = white
 posx = 300
 posy = 200
 circle = pygame.draw.circle(screen, colorcircle, (posx, posy), 50)
 
-while run_me:
-    clock.tick(fps_limit) 
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run_me = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                posx = posx - 10
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                posx = posx + 10
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                posy = posy - 10
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_DOWN:
-                posy = posy + 10
-    # fill the screen with black (otherwise, the circle will leave a trail)
-    screen.fill(black)
-    # redraw the circle
-    pygame.draw.circle(screen, colorcircle, (posx, posy), 50)
-    pygame.display.flip()
+def play_game():
 
-pygame.quit()
+    global posx, posy
+
+    clock = pygame.time.Clock()
+
+    # set_random_position()
+
+    run_me = True
+
+    while run_me:
+        clock.tick(60)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run_me = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    posx = posx - 10
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    posx = posx + 10
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    posy = posy - 10
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_DOWN:
+                    posy = posy + 10
+        # fill the screen with black (otherwise, the circle will leave a trail)
+        screen.fill(black)
+        # redraw the circle
+        pygame.draw.circle(screen, colorcircle, (posx, posy), 50)
+        pygame.display.flip()
+
+
+
+def main():
+
+    pygame.init()
+    pygame.display.set_caption('Hot Cold Game')
+
+    play_game()
+
+    pygame.quit()
+
+
+if __name__ == '__main__':
+    main()
