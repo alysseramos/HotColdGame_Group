7568edbf80d9a614ed6ec7454826ac8d7f14eb13
RohanOelofse <rohanoelofse1@gmail.com>
2023-03-21 10:19:23 -0500

    Got set_random_position working

--- HotColdGame.py
+++ HotColdGame.py
@@ -11,46 +11,47 @@ white = (255, 255, 255)
 screen_size = screen_width, screen_height = 800, 800
 screen = pygame.display.set_mode(screen_size)
 
-
-
 # fps_limit = 60
 
+SCREEN_SIZE = 800
 colorcircle = white
 hidden_color = black
 posx = 400
 posy = 400
-circle = pygame.draw.circle(screen, colorcircle, (posx, posy), 50)
-hidden_circle = pygame.draw.circle(screen, hidden_color, (200, 200), 50)
+circle_size = 50
+hidden_x = 200
+hidden_y = 200
+# circle = pygame.draw.circle(screen, colorcircle, (posx, posy), 50)
+# pygame.draw.circle(screen, hidden_color, (hidden_x, hidden_y), circle_size)
 
 
-'''
 def set_random_position():
 
-global game
+    global circle_size, hidden_x, hidden_y
 
-user_pos = screen_size // 2
+    user_pos = SCREEN_SIZE // 2
 
-inside_dist = hidden_circle['circle_size']
-outside_dist = screen_size - hidden_circle['circle_size']
+    inside_dist = circle_size
+    outside_dist = SCREEN_SIZE - circle_size
 
-right_user_dist = user_pos - hidden_circle['circle_size']
-left_user_dist = user_pos + hidden_circle['circle_size']
+    right_user_dist = user_pos - circle_size
+    left_user_dist = user_pos + circle_size
 
-while True:
+    while True:
 
-    posx = random.randint(inside_dist, outside_dist)
-    posy = random.randint(inside_dist, outside_dist)
+        x = random.randint(inside_dist, outside_dist)
+        y = random.randint(inside_dist, outside_dist)
 
-    if (posx < right_user_dist or posx > left_user_dist) and (posy < right_user_dist or posy > left_user_dist):
-        hidden_circle['hidden_x'] = posx
-        hidden_circle['hidden_y'] = posy
-        return
-'''
+        if (x < right_user_dist or x > left_user_dist) and (y < right_user_dist or y > left_user_dist):
+            hidden_x = x
+            hidden_y = y
+            return
+
 
 
 def play_game():
 
-    global posx, posy, hidden_color
+    global posx, posy, hidden_color, hidden_x, hidden_y
 
     clock = pygame.time.Clock()
 
@@ -84,9 +85,9 @@ def play_game():
         # fill the screen with black (otherwise, the circle will leave a trail)
         screen.fill(black)
         # redraw the circle
-        pygame.draw.circle(screen, colorcircle, (posx, posy), 50)
+        pygame.draw.circle(screen, colorcircle, (posx, posy), circle_size)
 
-        pygame.draw.circle(screen, hidden_color, (200, 200), 50)
+        pygame.draw.circle(screen, hidden_color, (hidden_x, hidden_y), circle_size)
         pygame.display.flip()
 
 
@@ -94,7 +95,7 @@ def main():
 
     pygame.init()
     pygame.display.set_caption('Hot Cold Game')
-
+    set_random_position()
     play_game()
 
     pygame.quit()
